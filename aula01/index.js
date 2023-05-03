9da3ced21d5a88e4d16ea1bf7121f6f5c3cad94c
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2023-05-03 19:45:34 -0300

    Create index.js
    TDE-02

---
+++ aula01/index.js
@@ -0,0 +1,35 @@
+let a = 20;
+let b = 30;
+
+
+function somar(){
+    console.log('A soma dele é '+ (a+b))
+}
+function subtrair(){
+    console.log('A subtração é '+ (a - b))
+}
+function dividir(){
+    console.log('A divisão é '+ (a%b))
+}
+function multiplicar(){
+    console.log('A multiplicação é '+ (a*b))
+}
+function soma(num1, num2){
+    console.log('A soma é '+(num1+num2))
+}
+function numero(valor){
+    if(valor %2 ===0){
+        console.log('O número é Par '+valor)
+    }else{
+        console.log('O número é ímpar '+valor)
+    }
+}
+
+
+somar();
+subtrair();
+dividir();
+multiplicar();
+
+soma(80,30)
+numero(25);
