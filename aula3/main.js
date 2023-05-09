46baa771e80b041b0b14a440fbd7b7506a6df6fa
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2023-05-09 20:37:31 -0300

    tde04

---
+++ aula3/main.js
@@ -0,0 +1,6 @@
+const calculadora = require("./calculadora"); //importar
+
+console.log(calculadora.soma(1,2));
+console.log(calculadora.subtracao(60,89));
+console.log(calculadora.mult(3,5));
+console.log(calculadora.div(100,45));
